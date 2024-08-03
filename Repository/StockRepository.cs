146202883fd463630fb5560a4bfbe0e94bb91eb8
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-08-03 13:59:02 +0300

    sorting

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -30,6 +30,26 @@ namespace StockAPI.Repository
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecsending
+                        ? stocks.OrderByDescending(s => s.Symbol)
+                        : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("CompanyName", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecsending
+                        ? stocks.OrderByDescending(s => s.CompanyName)
+                        : stocks.OrderBy(s => s.CompanyName);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
